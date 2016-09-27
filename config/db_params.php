ee480c349c1b12ed888b9508dd19ca740b10580c
Andrey <andrej.soldatov.88@gmail.com>
2016-09-27 13:46:52 +0300

    Add files via upload

---
+++ config/db_params.php
@@ -0,0 +1,9 @@
+<?php
+
+// Массив с параметрами подключения к базе данных
+return array(
+    'host' => 'mysql.hostinger.com.ua',
+    'dbname' => 'u678640596_shop',
+    'user' => 'u678640596_root',
+    'password' => 'A52031s1',
+);
---
+++ views/admin/index.php
@@ -0,0 +1,28 @@
+<?php include ROOT . '/views/layouts/header_admin.php'; ?>
+
+<section xmlns="http://www.w3.org/1999/html">
+    <div class="container">
+        <div class="row">
+            
+            <br/>
+            
+            <h4>Добрый день, администратор!</h4>
+            
+            <br/>
+            
+            <p>Вам доступны такие возможности:</p>
+            
+            <br/>
+            
+            <ul>
+                <li><a href="/admin/product">Управление товарами</a></li><br>
+                <li><a href="/admin/category">Управление категориями</a></li><br>
+                <li><a href="/admin/order">Управление заказами</a></li><br>
+            </ul>
+            
+        </div>
+    </div>
+</section>
+
+<?php include ROOT . '/views/layouts/footer_admin.php'; ?>
+
